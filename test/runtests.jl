c28cd95c570e7702ec9ab6df43c1b95c35104eac
Austin Privett <aprivett5192@gmail.com>
2017-05-24 12:58:01 -0500

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,16 @@ using MatrixChainMultiply
 using Base.Test
 
 # case on readme
-function readme_example()
-  a = rand(1000,1000)
-  b = rand(1000,100)
-  c = rand(100, 500)
-  d = rand(500)
+function readme_example(use_arrayfire=false)
+  if use_arrayfire
+    T = AFArray{Float64}
+  else
+    T = Float64
+  end
+  a = rand(T, 1000, 1000)
+  b = rand(T, 1000, 100)
+  c = rand(T, 100, 500)
+  d = rand(T, 500)
 
   # get results
   result1 = *(a,b,c,d)
