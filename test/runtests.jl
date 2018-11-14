5ce5977cd8cd0943f84a220bf4c42cd49844dfbd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-13 20:19:58 -0500

    use BenchmarkTools, increase speedup factor in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MatrixChainMultiply
-using Base.Test
+using BenchmarkTools
+using Test
 
 # case on readme
 function readme_example(use_arrayfire=false)
@@ -14,20 +15,19 @@ function readme_example(use_arrayfire=false)
   d = rand(T, 500)
 
   # get results
-  result1 = *(a,b,c,d)
-  result2 = matrixchainmultiply(a,b,c,d)
+  result1 = *(a, b, c, d)
+  result2 = matrixchainmultiply(a, b, c, d)
   difference = result1 - result2
 
   # do again to get timings
-  tic(); *(a,b,c,d); stdtime = toc()
-  tic(); matrixchainmultiply(a,b,c,d); chaintime = toc()
+  stdtime = @belapsed *($a, $b, $c, $d);
+  chaintime = @belapsed matrixchainmultiply($a, $b, $c, $d);
   speedup = stdtime / chaintime
-  print("Speedup = $speedup")
 
-  difference, speedup
+  return difference, speedup
 end
 
 difference, speedup = readme_example()
 
 @test norm(difference) < 1e-5
-@test speedup > 1.0
+@test speedup > 1.5
